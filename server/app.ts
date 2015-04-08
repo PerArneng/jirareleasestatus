6ca780d56b6f25e6c6c462f1f8ded540a94d4f7d
Per Arneng <per.arneng@scalebit.com>
2015-04-08 21:32:43 +0200

    initial checkin

---
+++ server/app.ts
@@ -0,0 +1,18 @@
+///<reference path='lib/node.d.ts' />
+///<reference path='lib/express.d.ts' />
+
+import express = require('express');
+var app:express.Application = express();
+
+app.get('/', (req:express.Request, res:express.Response) => {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
